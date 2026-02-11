c2e3ffd63c9ffee442e5b9de749eff3595cf8b88
victorspg <165102012+victorspg@users.noreply.github.com>
2026-02-11 06:29:49 +0000

    fix: simplify command parameters in create_adls_connection.ps1 for clarity

--- scripts/create_adls_connection.ps1
+++ scripts/create_adls_connection.ps1
@@ -12,13 +12,4 @@ Write-Host "Creating Fabric ADLS Gen2 connection: $ConnectionName"
 $server = "https://$StorageAccount.dfs.core.windows.net"
 $path = "/"
 
-fab create ".connections/$ConnectionName.Connection" -P connectionDetails.type=AzureDataLakeStorage,connectionDetails.creationMethod=AzureDataLakeStorage,credentialDetails.type=ServicePrincipal,credentialDetails.tenantId=$TenantId,credentialDetails.servicePrincipalClientId=$ClientId,credentialDetails.servicePrincipalSecret=$ClientSecret
-
-
-    connectionDetails.connectivityType=ShareableCloud,`
-    connectionDetails.parameters.server=$server,`
-    connectionDetails.parameters.path=$path,`
-    credentialDetails.type=ServicePrincipal,`
-    credentialDetails.tenantId=$TenantId,`
-    credentialDetails.servicePrincipalClientId=$ClientId,`
-    credentialDetails.servicePrincipalSecret=$ClientSecret
+fab create ".connections/$ConnectionName.Connection" -P connectionDetails.type=AzureDataLakeStorage,connectionDetails.creationMethod=AzureDataLakeStorage,credentialDetails.type=ServicePrincipal,credentialDetails.tenantId=$TenantId,credentialDetails.servicePrincipalClientId=$ClientId,credentialDetails.servicePrincipalSecret=$ClientSecret,connectionDetails.parameters.server=$server,connectionDetails.parameters.path=$path
