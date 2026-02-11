a3113a11a308b8a04c783a013f1bf257301f19b2
victorspg <165102012+victorspg@users.noreply.github.com>
2026-02-11 05:14:22 +0000

    fix: add https prefix to StorageAccount URL in ADLS Gen2 connection script

--- scripts/create_adls_connection.ps1
+++ scripts/create_adls_connection.ps1
@@ -9,7 +9,7 @@ param (
 
 Write-Host "Creating Fabric ADLS Gen2 connection: $ConnectionName"
 
-$server = "$StorageAccount.dfs.core.windows.net"
+$server = "https://$StorageAccount.dfs.core.windows.net"
 $path = "/"
 
 fab create ".connections/$ConnectionName.Connection" -P connectionDetails.type=AzureDataLakeStorage,connectionDetails.creationMethod=AzureDataLakeStorage,connectionDetails.parameters.server=$server,connectionDetails.parameters.path=$path,credentialDetails.type=ServicePrincipal,credentialDetails.tenantId=$TenantId,credentialDetails.servicePrincipalClientId=$ClientId,credentialDetails.servicePrincipalSecret=$ClientSecret
