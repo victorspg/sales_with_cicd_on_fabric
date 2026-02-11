5bb4e5b3e3beb7f4fde0ed6f0e61246ccbc70b59
victorspg <165102012+victorspg@users.noreply.github.com>
2026-02-11 04:44:00 +0000

    path param updated

--- scripts/create_adls_connection.ps1
+++ scripts/create_adls_connection.ps1
@@ -10,6 +10,6 @@ param (
 Write-Host "Creating Fabric ADLS Gen2 connection: $ConnectionName"
 
 $server = "$StorageAccount.dfs.core.windows.net"
-$fullPath = "/"
+$path = "/"
 
-fab create ".connections/$ConnectionName.Connection" -P connectionDetails.type=AzureDataLakeStorage,connectionDetails.creationMethod=AzureDataLakeStorage,connectionDetails.parameters.server=$server,connectionDetails.parameters.fullPath=$fullPath,credentialDetails.type=ServicePrincipal,credentialDetails.tenantId=$TenantId,credentialDetails.servicePrincipalClientId=$ClientId,credentialDetails.service
+fab create ".connections/$ConnectionName.Connection" -P connectionDetails.type=AzureDataLakeStorage,connectionDetails.creationMethod=AzureDataLakeStorage,connectionDetails.parameters.server=$server,connectionDetails.parameters.path=$path,credentialDetails.type=ServicePrincipal,credentialDetails.tenantId=$TenantId,credentialDetails.servicePrincipalClientId=$ClientId,credentialDetails.service
